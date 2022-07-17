06e28779f610773b72dc066f7ab4af2a365ab412
lightsing <lightsing@users.noreply.github.com>
2022-07-17 15:49:23 +0000

    deploy: fa312cbc7baabb382919a389cda2569c795f2cbf

--- implementors/core/fmt/trait.Display.js
+++ implementors/core/fmt/trait.Display.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["comet_eventbus"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.62.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"comet_eventbus/struct.TopicKey.html\" title=\"struct comet_eventbus::TopicKey\">TopicKey</a>","synthetic":false,"types":["comet_eventbus::TopicKey"]}];
+implementors["comet_eventbus"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.62.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"comet_eventbus/struct.TopicKey.html\" title=\"struct comet_eventbus::TopicKey\">TopicKey</a>","synthetic":false,"types":["comet_eventbus::topic_key::TopicKey"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var sourcesIndex = {};
-sourcesIndex["comet_eventbus"] = {"name":"","files":["bridge.rs","impl_async.rs","lib.rs"]};
+sourcesIndex["comet_eventbus"] = {"name":"","files":["bridge.rs","event.rs","event_listener.rs","impl_async.rs","lib.rs","topic.rs","topic_key.rs"]};
 createSourceSidebar();
